324f48d893f4a63ad396749072d77a91462f390c
ren21GitHub <reynielobut21@gmail.com>
2024-08-27 23:19:03 +0800

    edit profilepic1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants"
-import profilePic   from "../assets/ren1x1new1.png"
+import profilePic   from "../assets/ren1x1new1.jpg"
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
